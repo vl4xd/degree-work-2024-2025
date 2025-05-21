6e87c97dc7cfe407b720ad783aeaf33abd491c03
Платонов Владислав <113058723+vl4xd@users.noreply.github.com>
2025-05-21 07:10:48 +0500

    -

--- backend/main.py
+++ backend/main.py
@@ -63,7 +63,6 @@ class SortName(str, Enum):
 
 @app.get('/seasons', response_class=JSONResponse, summary='Получение списка всех сезонов РПЛ', tags=['Сезоны'])
 async def get_seasons():
-    return {'a':'a'}
     try:
         return await AC.Season.get_season_list()
     except Exception as e:
