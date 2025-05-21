789c3a72fb80b72f7315c592b4354f9c1e6185aa
Платонов Владислав <113058723+vl4xd@users.noreply.github.com>
2025-05-21 05:53:19 +0500

    -

--- backend/collection/browser.py
+++ backend/collection/browser.py
@@ -54,7 +54,7 @@ class AsyncBrowserConnection:
         options = Options()
         # options.set_preference('dom.webdriver.enabled', False) # деактивация вебдрайвера
         # options.set_preference('media.volume_scale', '0.0')
-        # options.add_argument('--headless') # не запускать GUI браузера
+        options.add_argument('--headless') # не запускать GUI браузера
         options.add_argument('--disable-gpu')
         options.add_argument('--enable-unsafe-swiftshader')
         options.add_argument('--no-sandbox')
