8db7a976ab4006bbf40ae61cf11b040bfc8d1413
Платонов Владислав <113058723+vl4xd@users.noreply.github.com>
2025-05-21 06:37:48 +0500

    -

--- backend/main.py
+++ backend/main.py
@@ -63,7 +63,10 @@ class SortName(str, Enum):
 
 @app.get('/seasons', response_class=JSONResponse, summary='Получение списка всех сезонов РПЛ', tags=['Сезоны'])
 async def get_seasons():
-    return await AC.Season.get_season_list()
+    try:
+        return await AC.Season.get_season_list()
+    except Exception as e:
+        print(e)
 
 
 @app.get('/season/games', response_class=JSONResponse, summary='Получение списка матчей', tags=['Матчи'])
@@ -78,25 +81,34 @@ async def get_season_games(
     limit: Optional[int] = 5,
     offset: Optional[int] = 0,
 ):
-    return await AC.Game.get_game_list(season_id,
-                                        sort_type,
-                                        game_statuses,
-                                        left_team_id,
-                                        right_team_id,
-                                        from_start_date,
-                                        to_start_date,
-                                        limit,
-                                        offset)
+    try:
+        return await AC.Game.get_game_list(season_id,
+                                            sort_type,
+                                            game_statuses,
+                                            left_team_id,
+                                            right_team_id,
+                                            from_start_date,
+                                            to_start_date,
+                                            limit,
+                                            offset)
+    except Exception as e:
+        print(e)
     
     
 @app.get('/season/teams', response_class=JSONResponse, summary='Получение списка команд', tags=['Команды'])
 async def get_teams(season_id: str = None):
-    return await AC.SeasonTeam.get_season_team_list(season_id)
+    try:
+        return await AC.SeasonTeam.get_season_team_list(season_id)
+    except Exception as e:
+        print(e)
 
 
 @app.get('/season/game/prediction', summary='Получение списка предсказаний игры', tags=['Предсказания'])
 async def get_prediction(game_id: int, sort_type: SortName):
-    return await AC.PredictionDrawLeftRight.get_game_prediction(game_id, sort_type)
+    try:
+        return await AC.PredictionDrawLeftRight.get_game_prediction(game_id, sort_type)
+    except Exception as e:
+        print(e)
 
 
 if __name__=='__main__':
